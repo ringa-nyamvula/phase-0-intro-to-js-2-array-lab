aeee3cc611a4e57fe80a7160172dcfb2398140ba
nyamvularinga <nyamvularinga@gmail.com>
2023-03-09 15:11:26 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+const cats =["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat (name) {
+    cats.push(name)
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+function appendCat(name) {
+    const dog=[...cats,name]
+    return dog
+}
+function prependCat(name){
+    const cow=[name,...cats]
+    return cow
+}
+function removeLastCat(){
+    const goat= cats.slice(0,-1)
+    return goat
+}
+function removeFirstCat(){
+    const lion= cats.slice(1)
+    return lion
+}
